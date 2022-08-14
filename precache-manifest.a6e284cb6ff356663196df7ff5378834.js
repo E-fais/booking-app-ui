19d7e09b2367ddc3d174547a3e599d8f2518b260
fais edathil <faiesedathil@gmail.com>
2022-08-15 00:07:06 +0300

    Updates

---
+++ precache-manifest.a6e284cb6ff356663196df7ff5378834.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "82f3cbe5ace64318470f",
+    "url": "/booking-app-ui/static/css/main.72a31a27.chunk.css"
+  },
+  {
+    "revision": "82f3cbe5ace64318470f",
+    "url": "/booking-app-ui/static/js/main.82f3cbe5.chunk.js"
+  },
+  {
+    "revision": "cc6630ab0b05d0ee6ca1",
+    "url": "/booking-app-ui/static/js/1.cc6630ab.chunk.js"
+  },
+  {
+    "revision": "55daa50646f79d8aea6c",
+    "url": "/booking-app-ui/static/js/runtime~main.55daa506.js"
+  },
+  {
+    "revision": "b7ba9c66646c7f5d643b0c1dd3416887",
+    "url": "/booking-app-ui/static/media/travel.b7ba9c66.jpg"
+  },
+  {
+    "revision": "790d64a3bb58433fddbc56d22389a544",
+    "url": "/booking-app-ui/index.html"
+  }
+];
